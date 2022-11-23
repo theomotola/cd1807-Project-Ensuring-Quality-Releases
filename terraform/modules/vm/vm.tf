4ce1a91f7c4d3560fea4241c962504d094fed848
techjunkie <tolaniolowosule@gmail.com>
2022-11-23 00:43:52 +0000

    Update vm.tf

--- terraform/modules/vm/vm.tf
+++ terraform/modules/vm/vm.tf
@@ -21,7 +21,7 @@ resource "azurerm_linux_virtual_machine" "test" {
   network_interface_ids = [azurerm_network_interface.test.id]
   admin_ssh_key {
     username   = var.admin_username
-    public_key = file("$(udacity_public_key.secureFilePath)")
+    public_key = file(var.public_key_path)
   }
   os_disk {
     caching           = "ReadWrite"
